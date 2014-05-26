46b171ba62ba65f2fecfd3b345d4c78c9431b421
Emilleopold <manfredschuessler@me.com>
2014-05-26 23:34:53 +0200

    Weitere Versuche

--- Arduino_YUN_Beispiele/Bridge/Bridge.ino
+++ Arduino_YUN_Beispiele/Bridge/Bridge.ino
@@ -23,6 +23,8 @@ You can use the curl command from the command line instead of a browser if you p
 #include <YunServer.h>
 #include <YunClient.h>
 #include <HttpClient.h>
+#include <EEPROM.h>
+#include "Timer.h"
 
 // Listen on default port 5555, the webserver on the Yun
 // will forward there all the HTTP requests for us.
@@ -32,12 +34,22 @@ boolean Status = false;
 char Orderstring[255];
 char SysVarTemperatur[] = "Temperatur";
 float Temperatur = 0.0;
+unsigned long OldMilli = 0;
+
+int ledgn1 = 8; // 1. Grüne LED
+int ledrd1 = 9; // 1. Rote LED
+int ledgn2 = 10; // 2. Grüne LED
+int ledrd2 = 11; // 2. Rote LED
+int ledState1s = LOW;             // ledState used to set the LED in 1 Sec timer
+int ledStateLoop = LOW;             // ledState used to set the LED in loop()
+
+Timer t;
 
 
 void setup() {
   Serial.begin(9600);
   // Wait until a Serial Monitor is connected.
-//  while (!Serial);
+  // while (!Serial);
   Serial.println("Bridge Init starten");
   // Bridge startup
   pinMode(13,OUTPUT);
@@ -50,49 +62,60 @@ void setup() {
   // (no one from the external network could connect)
   server.listenOnLocalhost();
   server.begin();
+  
+    // initialize the digital pin as an output.
+  pinMode(ledgn1, OUTPUT);
+  pinMode(ledrd1, OUTPUT);
+  pinMode(ledgn2, OUTPUT);
+  pinMode(ledrd2, OUTPUT);
+
+  int tick1SekEvent = t.every(1000, doAll1Sek, (void*)2);
+  Serial.print("1 second tick started id=");
+  Serial.println(tick1SekEvent);
+
+  int tick10SekEvent = t.every(10000, doAll10Sek, (void*)2);
+  Serial.print("10 second tick started id=");
+  Serial.println(tick10SekEvent);
+
+  int tick1MinEvent = t.every(60000, doAll1Min, (void*)2);
+  Serial.print("1 minute tick started id=");
+  Serial.println(tick1MinEvent);
+
+  int tick1HourEvent = t.every(3600000, doAll1Hour, (void*)2); // 1h = 3.600.000 mSek
+  Serial.print("1 hour tick started id=");
+  Serial.println(tick1HourEvent);
+
+  int tick6HourEvent = t.every(21600000, doAll6Hour, (void*)2); // 6h = 21.600.000 mSek
+  Serial.print("6 hour tick started id=");
+  Serial.println(tick6HourEvent);
+
+  Temperatur = float(ReadCountEEProm(0));
 }
 
 void loop() {
+  //Serial.println();
+  //Serial.println("Hier startet loop()");
+  //TimeStamp("Yclient");
   // Get clients coming from server
   YunClient Yclient = server.accept();
-
+  //TimeStamp("accept");
   // There is a new client?
   if (Yclient) {
     // Process request
     process(Yclient);
-
     // Close connection and free resources.
     Yclient.stop();
   }
-
+  //TimeStamp("Stop");
   delay(50); // Poll every 50ms
+  t.update();
+  if (ledStateLoop == LOW)
+    ledStateLoop = HIGH;
+  else
+    ledStateLoop = LOW;
+  digitalWrite(ledgn1, ledStateLoop);
 
-/*
-  if (Status == false) {
-    client.get("http://192.168.11.220:8181/do.exe?r1=dom.GetObject(\"Temperatur\").State(\"50\")");
-    Status = true;
-    Serial.println("50 gesendet");
-    }
-  else {
-    client.get("http://192.168.11.220:8181/do.exe?r1=dom.GetObject(\"Temperatur\").State(\"10\")");
-    Status = false;
-    Serial.println("10 gesendet");
-    }
 
-*/  
-  HttpClient Hclient;
-  char Str1[20];
-  FloatToString(Temperatur, 100, Str1);
-  //fmtDouble(Temperatur, 2, *gleitpunkt); // produces 3.14 (two decimal places)
-
-  sprintf(Orderstring,"http://192.168.11.220:8181/do.exe?r1=dom.GetObject(\"%s\").State(\"%s\")",SysVarTemperatur, Str1);
-  Serial.println(Orderstring);
-  Serial.println(Temperatur);
-  //++Temperatur;
-  Temperatur = Temperatur + 0.12;
-  Hclient.get(Orderstring);
-
-  delay(10*1000); // alle 10 Sekunden
 }
 
 void process(YunClient client) {
@@ -242,4 +265,91 @@ void FloatToString( float val, unsigned int precision, char* Dest){
   strcat(Teil1, Teil2);
   strcpy(Dest, Teil1);
 } 
+
+
+void TimeStamp(char* Str) {
+  unsigned long Milli = millis();
+  Serial.print(Str);
+  Serial.print(" Differenzzeit [Sek] : ");
+  Serial.println(Milli - OldMilli);
+  OldMilli = Milli;
+}
+
+long ReadCountEEProm(int BaseAddress){
+  long Count = 0;
+  int Shift = BaseAddress + 3;
+  for (int i = BaseAddress; i < BaseAddress+4; i++) {
+    Count = Count << 8;
+    Count = Count | long(EEPROM.read(Shift));
+    Shift--;
+  }
+  return Count;
+}
+
+void WriteCountEEProm(long Count, int BaseAddress){
+  int ByteX;
+  for (int i = BaseAddress; i < BaseAddress+4; i++){
+    ByteX = int(Count & 0x000000FF);
+    EEPROM.write(i, ByteX);
+    Count = Count >> 8;
+  }
+}
+
+void EEPromWrite(void){
+  Serial.println("EEPROM geschrieben");
+  WriteCountEEProm(long(Temperatur) , 0);
+}
+
+void doAll1Sek(void* context) {
+  //int time = (int)context;
+  //Serial.print(time);
+  //Serial.print(" 1 second tick: millis()=");
+  //Serial.println(millis());
+  // set the LED with the ledState of the variable:
+  if (ledState1s == LOW)
+    ledState1s = HIGH;
+  else
+    ledState1s = LOW;
+  digitalWrite(ledrd1, ledState1s);
+}
+
+void doAll10Sek(void* context) {
+  int time = (int)context;
+  //Serial.print(time);
+  //Serial.print(" 10 second tick: millis()=");
+  //Serial.println(millis());
+  HttpClient Hclient;
+  TimeStamp("Hclient");
+  char Str1[20];
+  FloatToString(Temperatur, 100, Str1);
+  sprintf(Orderstring,"http://192.168.11.220:8181/do.exe?r1=dom.GetObject(\"%s\").State(\"%s\")",SysVarTemperatur, Str1);
+  Serial.println(Orderstring);
+  Serial.println(Temperatur);
+  Temperatur = Temperatur + 0.12;
+  TimeStamp("Temp");
+  Hclient.get(Orderstring);
+  TimeStamp("Orderstring");
+}
+
+void doAll1Min(void* context) {
+  int time = (int)context;
+  //Serial.print(time);
+  //Serial.print(" 1 minute tick: millis()=");
+  //Serial.println(millis());
+}
+
+void doAll1Hour(void* context) {
+  int time = (int)context;
+  Serial.print(time);
+  Serial.print(" 1 hour tick: millis()=");
+  Serial.println(millis());
+  EEPromWrite();
+}
+
+void doAll6Hour(void* context) {
+  int time = (int)context;
+  Serial.print(time);
+  Serial.print(" 6 hour tick: millis()=");
+  Serial.println(millis());
+}
 
